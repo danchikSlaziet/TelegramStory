121921e0fefffc944d0c652b09adc5ff383d2da8
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2024-08-08 21:05:54 +0300

    fdf

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let userChatId;
+function parseQuery(queryString) {
+  let query = {};
+  let pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+  for (let i = 0; i < pairs.length; i++) {
+      let pair = pairs[i].split('=');
+      query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  }
+  return query;
+}
 document.addEventListener('DOMContentLoaded', () => {
   let app = window.Telegram.WebApp;
   let query = app.initData;
