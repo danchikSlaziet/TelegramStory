1be3cec8826fa0aa9ff6d6461e62b1984ed6a2a0
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2024-08-08 21:03:00 +0300

    f

---
+++ script.js
@@ -0,0 +1,15 @@
+let userChatId;
+document.addEventListener('DOMContentLoaded', () => {
+  let app = window.Telegram.WebApp;
+  let query = app.initData;
+  let user_data_str = parseQuery(query).user;
+  let user_data = JSON.parse(user_data_str);
+  app.expand();
+  app.ready();
+  userChatId = user_data["id"];
+  document.getElementById('buttonStory').addEventListener("click", () => {
+    app.shareToStory('https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Example_image.svg/600px-Example_image.svg.png')
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+  })
+});
