5602aa7a51f47c4ccea35368d826bb55ff228473
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2024-08-08 21:13:21 +0300

    dfdf

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();
 let userChatId;
 function parseQuery(queryString) {
   let query = {};
